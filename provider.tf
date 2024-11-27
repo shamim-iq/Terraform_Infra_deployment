ca2c5e6a8be4c9813b8b8bf8234144592ae8a65f
Ubuntu <ubuntu@ip-172-31-24-162.ec2.internal>
2024-11-27 22:29:34 +0000

    provider modified

--- provider.tf
+++ provider.tf
@@ -1,9 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "5.78.0"
     }
+  }
+  backend "s3" {
+    bucket = "remotebackendbucketshamim"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
   }
 }
 
